63c8e24e6e949f120520021cbd0d6bf3db69cb8a
AmberSaxena0 <117968525+AmberSaxena0@users.noreply.github.com>
2024-09-04 14:02:22 +0530

    Design schemas and table for podcasts and users. Setup is done for clerk and convex for user authentication and backend services

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,10 +6,10 @@ import PodcastCard from "@/components/PodcastCard";
 
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
-// import { api } from "../convex/_generated/api";
 
 const Home = () => {
   const tasks = useQuery(api.tasks.get);
+  console.log(tasks, "tasks");
 
   return (
     <div className="mt-9 flex flex-col gap-9">
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://romantic-termite-56.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
