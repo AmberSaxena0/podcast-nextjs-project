7ef80318c45aaeecc2a6afdc4193904d2d061017
AmberSaxena0 <117968525+AmberSaxena0@users.noreply.github.com>
2024-09-08 02:05:22 +0530

    Started working on create podcast form. Authentication done

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,7 +1,140 @@
-import React from "react";
+// "use client";
+
+// import React from "react";
+
+// import { z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+
+// import { Button } from "@/components/ui/button";
+// import {
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { Form, useForm } from "react-hook-form";
+
+// const formSchema = z.object({
+//   username: z.string().min(2).max(50),
+// });
+
+// const CreatePodcast = () => {
+//   const form = useForm<z.infer<typeof formSchema>>({
+//     resolver: zodResolver(formSchema),
+//     defaultValues: {
+//       username: "",
+//     },
+//   });
+
+//   // 2. Define a submit handler.
+//   function onSubmit(values: z.infer<typeof formSchema>) {
+//     // Do something with the form values.
+//     // ✅ This will be type-safe and validated.
+//     console.log(values);
+//   }
+
+//   return (
+//     <Form {...form}>
+//       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+//         <FormField
+//           control={form.control}
+//           name="username"
+//           render={({ field }) => (
+//             <FormItem>
+//               <FormLabel>Username</FormLabel>
+//               <FormControl>
+//                 <Input placeholder="shadcn" {...field} />
+//               </FormControl>
+//               <FormDescription>
+//                 This is your public display name.
+//               </FormDescription>
+//               <FormMessage />
+//             </FormItem>
+//           )}
+//         />
+//         <Button type="submit">Submit</Button>
+//       </form>
+//     </Form>
+//   );
+// };
+
+// export default CreatePodcast;
+
+"use client";
+
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 const CreatePodcast = () => {
-  return <h1 className="text-20 font-bold text-white-1">CreatePodcast </h1>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <section className="mt-10 flex flex-col">
+      <h1 className="text-20 font-bold text-white-1">CreatePodcast </h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full flex flex-col mt-12"
+        >
+          <FormField
+            control={form.control}
+            name="podcastTitle"
+            render={({ field }) => (
+              <FormItem className="flex flex-col gap-2.5 ">
+                <FormLabel className="text-16 font-bold text-white-1">
+                  Username
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="input-class focus-visible:ring-orange-1"
+                    placeholder="VocalVibes"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage className="text-white-1" />
+              </FormItem>
+            )}
+          />
+          <div className=" flex flex-col gap-[30px] border-b border-black-5 pb-10"></div>
+        </form>
+      </Form>
+    </section>
+  );
 };
 
 export default CreatePodcast;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,17 +8,12 @@ import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 
 const Home = () => {
-  const tasks = useQuery(api.tasks.get);
-  console.log(tasks, "tasks");
-
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Vocals </h1>
 
         <div className="podcast_grid text-white-1">
-          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-
           {podcastData.map(({ id, description, imgURL, title }) => {
             return (
               <PodcastCard
